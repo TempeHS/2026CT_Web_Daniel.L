c3ff4b7109239d2ced861f93ace0854a36cc403b
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-03-11 00:00:53 +0000

    rock paper scissors

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,43 @@
-rock = 1;
+scissors = 1;
 paper = 2;
-scissors = 3;
+rock = 3;
 
-player_name = prompt("enter your name");
+player_name = prompt("Enter your name:");
 alert("Hello " + player_name);
-player_guess = prompt("rock paper scissors");
+
+player_guess = prompt("Choose scissors, paper, or rock:").toLowerCase();
+
 computer_guess = randomIntFromInterval(1, 3);
 
-if (player_guess == ) {
+let computer_choice = "";
+if (computer_guess === scissors) computer_choice = "scissors";
+if (computer_guess === paper) computer_choice = "paper";
+if (computer_guess === rock) computer_choice = "rock";
+
+if (
+  (player_guess === "scissors" && computer_guess === scissors) ||
+  (player_guess === "paper" && computer_guess === paper) ||
+  (player_guess === "rock" && computer_guess === rock)
+) {
   document.getElementById("user_feedback").innerHTML =
-    "Correct, i know your cheating";
+    "It's a tie! Both chose " + computer_choice;
+} else if (
+  (player_guess === "scissors" && computer_guess === paper) ||
+  (player_guess === "paper" && computer_guess === rock) ||
+  (player_guess === "rock" && computer_guess === scissors)
+) {
+  document.getElementById("user_feedback").innerHTML =
+    "You win! Computer chose " + computer_choice;
+} else if (
+  (player_guess === "scissors" && computer_guess === rock) ||
+  (player_guess === "paper" && computer_guess === scissors) ||
+  (player_guess === "rock" && computer_guess === paper)
+) {
+  document.getElementById("user_feedback").innerHTML =
+    "You lose! Computer chose " + computer_choice;
 } else {
   document.getElementById("user_feedback").innerHTML =
-    "You suck, the number was " + computer_guess;
+    "Invalid input! Please enter scissors, paper, or rock.";
 }
 
 function randomIntFromInterval(min, max) {
