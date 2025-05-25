eb6798dcc4ddfddf3f725cf31773816b2c03e682
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-05-25 23:36:07 +0000

    added code

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 def index():
     card_data = (
-        {"What is Google", "Description", "BUtton text", "static/images/card_image1.png"},
-        {"Why use google", "Description", "BUtton text", "static/images/card_image2.png"},
-        {"", "Description", "BUtton text", "static/images/card_image3.png"},      
+        ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png"),
+        ("Why use google", "Description", "BUtton text", "static/images/card_image2.png"),
+        ("Features and services", "Description", "BUtton text", "static/images/card_image3.png"),      
     )
-    return render_template("index.html"), 200
+    return render_template("index.html", cards=card_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
