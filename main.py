6f0a5bcf77d1ce247236140a0d96f33a1f3f5d62
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-05-27 23:39:10 +0000

    stuff

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def index():
     card_data = (
         ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png"),
-        ("Why use google", "Description", "BUtton text", "static/images/card_image2.png"),
-        ("Features and services", "Description", "BUtton text", "static/images/card_image3.png"),      
+        ("Why use google", "Google offers fast, reliable, and comprehensive search results, making it easy to find exactly what you need. Its user-friendly interface and advanced algorithms ensure you get the most relevant information in seconds.", "BUtton text", "static/images/card_image2.png"),
+        ("Features and services", "Google provides a wide range of features and services, including Gmail, Google Maps, Google Drive, and YouTube. These tools help users communicate, navigate, store files, and access entertainment, all seamlessly integrated with your Google account.", "BUtton text", "static/images/card_image4.png")
     )
     return render_template("index.html", cards=card_data), 200
 
