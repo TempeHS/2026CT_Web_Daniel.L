1dd7005e42da030a4ffcdc77129a4d6e6081fd94
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-06-02 02:49:01 +0000

    added new pages

--- main.py
+++ main.py
@@ -8,11 +8,26 @@ app = Flask(__name__)
 
 def index():
     card_data = (
-        ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png"),
-        ("Why use google", "Google offers fast, reliable, and comprehensive search results, making it easy to find exactly what you need. Its user-friendly interface and advanced algorithms ensure you get the most relevant information in seconds.", "BUtton text", "static/images/card_image2.png"),
-        ("Features and services", "Google provides a wide range of features and services, including Gmail, Google Maps, Google Drive, and YouTube. These tools help users communicate, navigate, store files, and access entertainment, all seamlessly integrated with your Google account.", "BUtton text", "static/images/card_image4.png")
+        ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png", "/steps/intro"),
+        ("Why use google", "Google offers fast, reliable, and comprehensive search results, making it easy to find exactly what you need. Its user-friendly interface and advanced algorithms ensure you get the most relevant information in seconds.", "BUtton text", "static/images/card_image2.png", "https://getbootstrap.com/"),
+        ("Features and services", "Google provides a wide range of features and services, including Gmail, Google Maps, Google Drive, and YouTube. These tools help users communicate, navigate, store files, and access entertainment, all seamlessly integrated with your Google account.", "BUtton text", "static/images/card_image4.png", "https://getbootstrap.com/")
     )
     return render_template("index.html", cards=card_data), 200
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html"), 200
+
+@app.route('/steps')
+@app.route('/help')
+def steps():
+    return render_template("steps.html"), 200
+
+@app.route('/steps/intro')
+def steps_intro():
+    return render_template("/steps/intro.html"), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
