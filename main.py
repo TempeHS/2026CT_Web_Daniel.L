e6a1066f2d62d4b76df3b30ab014817e9293fe07
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-06-22 23:40:35 +0000

    added

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 def index():
     card_data = (
-        ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png", "/steps/intro"),
-        ("Why use google", "Google offers fast, reliable, and comprehensive search results, making it easy to find exactly what you need. Its user-friendly interface and advanced algorithms ensure you get the most relevant information in seconds.", "BUtton text", "static/images/card_image2.png", "https://getbootstrap.com/"),
-        ("Features and services", "Google provides a wide range of features and services, including Gmail, Google Maps, Google Drive, and YouTube. These tools help users communicate, navigate, store files, and access entertainment, all seamlessly integrated with your Google account.", "BUtton text", "static/images/card_image4.png", "https://getbootstrap.com/")
+        ("What is Google", "Google is a powerful search engine that helps users find information quickly and efficiently. From websites and images to news and videos, Google organizes the world data to make it accessible at the click of a button.", "BUtton text", "static/images/card_image1.png", "/steps/intro", "enabled"),
+        ("Why use google", "Google offers fast, reliable, and comprehensive search results, making it easy to find exactly what you need. Its user-friendly interface and advanced algorithms ensure you get the most relevant information in seconds.", "BUtton text", "static/images/card_image2.png", "https://getbootstrap.com/", "disabled"),
+        ("Features and services", "Google provides a wide range of features and services, including Gmail, Google Maps, Google Drive, and YouTube. These tools help users communicate, navigate, store files, and access entertainment, all seamlessly integrated with your Google account.", "BUtton text", "static/images/card_image4.png", "https://getbootstrap.com/", "disabled")
     )
     return render_template("index.html", cards=card_data), 200
 
@@ -63,6 +63,10 @@ def steps_4():
 @app.route('/steps/5')
 def steps_5():
     return render_template("/steps/step_5.html"), 200
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("pagenotfound.html"), 404
 
 
 
